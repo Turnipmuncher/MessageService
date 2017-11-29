c48ae60c47db346519ec0e30b5522855e5ca7ae3
Turnipmuncher <q5233000@live.tees.ac.uk>
2017-11-29 21:01:32 +0000

    I hate you so much

---
+++ MessagingService/MessagingService/Controllers/MessageController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MessagingService.Respsitory;
+using MessagingService.Models;
+using System.Collections;
+
+namespace MessagingService.Controllers
+{
+    [Route("api/message")]
+    public class MessageController : Controller
+    {
+        public IMessageRepository MessageRepo { get; set; }
+
+        public MessageController(IMessageRepository _repo)
+        {
+            MessageRepo = _repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> GetAll()
+        {
+
+            return MessageRepo.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public IActionResult GetById(string id)
+        {
+            var item = MessageRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+        [HttpGet]
+        [Route("api/invoice")]
+        public IEnumerable GetInvoice()
+        {
+            string subject = "invoice";
+
+            List<Message> InvoiceList = MessageRepo.GetAll().ToList();
+
+            return InvoiceList.Where(e => e.subject.Equals(subject));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Message item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            MessageRepo.Add(item);
+            return CreatedAtRoute("GetMessage", new { Controller = "Message", id = item.id }, item);
+        }
+
+        //[HttpPost]
+        //[Route("api/invoice")]
+        //public IActionResult createinvoice([FromBody] Message item)
+        //{
+        //    if (item == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    item.subject = "invoice";
+        //    MessageRepo.Add(item);
+        //    return CreatedAtRoute("GetMessage", new { Controller = "Message", id = item.id}, item);
+        //}
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] Message item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            var contactObj = MessageRepo.Find(id);
+            if (contactObj == null)
+            {
+                return NotFound();
+            }
+            MessageRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            MessageRepo.remove(id);
+        }
+    }
+}
+
--- MessagingService/MessagingService/Startup.cs
+++ MessagingService/MessagingService/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MessagingService.Respsitory;
+using MessagingService.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace MessagingService
 {
@@ -24,6 +27,13 @@ namespace MessagingService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<MessageContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //var connection = @"Server=(localdb)\mssqllocaldb;Database=MessageDB;Trusted_Connection=True;";
+            //services.AddDbContext<MessageContext>(options => options.UseSqlServer(connection));
+
+            services.AddTransient<IMessageRepository, MessageRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
